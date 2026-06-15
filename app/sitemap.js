76cd1b9b27adc7207160b04af67bbb1e65abbdb9
dflaggvt <dflagg@gmail.com>
2026-06-14 23:24:45 -0400

    Add backlink link resources

--- app/sitemap.js
+++ app/sitemap.js
@@ -51,6 +51,12 @@ export default function sitemap() {
       priority: 0.7,
     },
     {
+      url: `${baseUrl}/link-to-us`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
       url: `${baseUrl}/legal/terms`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
